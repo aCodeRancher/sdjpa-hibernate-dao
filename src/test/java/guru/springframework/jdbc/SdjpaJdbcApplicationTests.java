4e571099f32a93bb3eeb4a5a40a415d0389030d9
helen <hhma@alumni.cpp.edu>
2022-10-06 23:18:04 -0400

    add test with h2

--- src/test/java/guru/springframework/jdbc/SdjpaJdbcApplicationTests.java
+++ src/test/java/guru/springframework/jdbc/SdjpaJdbcApplicationTests.java
@@ -1,15 +1,42 @@
 package guru.springframework.jdbc;
 
-import org.junit.jupiter.api.Disabled;
+import guru.springframework.jdbc.dao.AuthorDao;
+import guru.springframework.jdbc.dao.AuthorDaoImpl;
+import guru.springframework.jdbc.domain.Author;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@Disabled // no h2 context in project
-@SpringBootTest
+
+@DataJpaTest
+@Import({AuthorDaoImpl.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SdjpaJdbcApplicationTests {
 
+	@Autowired
+	AuthorDao authorDao;
+
 	@Test
-	void contextLoads() {
+	void testFindAllAuthors() {
+		List<Author> authors = authorDao.findAll();
+
+		assertThat(authors).isNotNull();
+		assertThat(authors.size()).isEqualTo(3);
+	}
+
+	@Test
+	void testSaveAuthor() {
+		Author author = new Author();
+		author.setFirstName("Peter");
+		author.setLastName("Schor");
+		Author saved = authorDao.saveNewAuthor(author);
+
+		assertThat(saved).isNotNull();
+		assertThat(saved.getId()).isNotNull();
 	}
 
 }
